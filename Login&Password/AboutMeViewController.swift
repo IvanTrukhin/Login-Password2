73b0edacb919cb885a3000819150adbbfa1cefc7
IvanTrukhin <trukhinivan25@gmail.com>
2023-05-17 15:37:43 +0500

    Alert Controller

--- Login&Password/AboutMeViewController.swift
+++ Login&Password/AboutMeViewController.swift
@@ -10,5 +10,10 @@ import UIKit
 
 class AboutMeViewController: UIViewController {
     
+    var user: User!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = user.person.fullName
+    }
 }
--- Login&Password/LoginViewController.swift
+++ Login&Password/LoginViewController.swift
@@ -24,16 +24,37 @@ class LoginViewController: UIViewController {
         let tabBarController = segue.destination as! UITabBarController
         let welcomeVC = tabBarController.viewControllers?.first as! WelcomeViewController
         let navigationVC = tabBarController.viewControllers?.last as! UINavigationController
-        
+        let aboutUserVC = navigationVC.topViewController as! AboutMeViewController
+        welcomeVC.user = user
+        aboutUserVC.user = user
+    }
+    
+    // MARK: IBActions
+    
+    @IBAction func LogInPressed() {
+        guard
+            userNameTextField.text == user.login,
+            passwordTextField.text == user.password
+        else {
+            showAlert(title: "Invalid login or password", messege: "Please, enter correct login and password", textField: passwordTextField)
+        return
+        }
+        performSegue(withIdentifier: "logIn", sender: nil)
     }
     
     
     
     
-    
-    
-    
-    
-    
 }
+    
+    // MARK: - Alert Controller
+    extension LoginViewController {
+        private func showAlert(title: String, messege: String, textField: UITextField? = nil) {
+            let alert = UIAlertController(title: title, message: messege, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default) { _ in textField?.text = nil
+            }
+            alert.addAction(okAction)
+            present(alert, animated: true)
+        }
+    }
 
